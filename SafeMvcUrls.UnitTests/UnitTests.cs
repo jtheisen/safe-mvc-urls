658461932e467bdb446920fa9d5658c707880f27
jens <jens.theisen@monkeybusters.de>
2016-06-17 23:36:43 +0200

    Tests, also nuspec in both solution and project folder as it didn't get picked up by appveyor.

---
+++ SafeMvcUrls.UnitTests/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronStone.Web.Mvc.Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void RunAllTests()
+        {
+            Tests.RunTests();
+        }
+    }
+}
