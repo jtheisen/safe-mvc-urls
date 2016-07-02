79d0c165ac321b3143773ac96ca7589c7666d041
jens <jens.theisen@monkeybusters.de>
2016-07-02 23:20:54 +0200

    AppVeyor versioning.

--- SafeMvcUrls/Properties/AssemblyInfo.cs
+++ SafeMvcUrls/Properties/AssemblyInfo.cs
@@ -22,15 +22,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("48131684-36ae-4060-be6f-bccb7a00bb70")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+// Set by AppVeyor.
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
